dfa06d28eb54088ece5817464e9bd71a5b78e46f
Lee Kyuhong <6puritans9@gmail.com>
2025-03-09 23:32:38 +0900

    chore: remove unused component and diable eslint

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,9 @@ const nextConfig: NextConfig = {
     images: {
         domains: ["ukdppxnanimydpuedftw.supabase.co"],
     },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
 };
 
 export default nextConfig;
