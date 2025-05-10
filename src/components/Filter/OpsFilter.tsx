078e13cc8a6321bc744b9a39f3fb8cb982b96d8a
Lee Kyuhong <6puritans9@gmail.com>
2025-05-10 19:21:23 +0900

    feat: enhance OpsFilter component with improved layout and hover functionality

--- src/components/Filter/OpsFilter.tsx
+++ src/components/Filter/OpsFilter.tsx
@@ -4,14 +4,73 @@ import { useState } from "react";
 import Image from "next/image";
 import { OpsFilterCondition } from "@/lib/types";
 import { classMap, factionMap } from "@/lib/constants/pathnameMap";
-import { BranchList } from "./BranchList";
+import BranchList from "./BranchList";
+import { HoverCard } from "radix-ui";
 import getS3Url from "@/lib/apiAws";
+import { flex, grid } from "../../../styled-system/patterns";
+import { css } from "../../../styled-system/css";
 
+// Styles
+const filterSection = flex({
+    gap: "1.5",
+});
+
+const filterTitle = css({
+    fontSize: {
+        base: "sm",
+        md: "lg",
+    },
+});
+
+const rarityWrapper = flex({
+    justify: "spaceBetween",
+    align: "center",
+    cursor: "pointer",
+    gap: "0.2rem",
+});
+
+const classWrapper = flex({
+    cursor: "pointer",
+});
+
+const factionWrapper = grid({
+    gridTemplateColumns: "repeat(10, 1fr)",
+    gridTemplateRows: "auto",
+    gap: "0",
+    justifyItems: "center",
+    alignItems: "center",
+    cursor: "pointer",
+});
+
+const factionImage = css({
+    height: "30px",
+    width: "auto",
+    backgroundColor: "black",
+});
+
+const popUpWrapper = css({
+    backgroundColor: "rgba(0,0,0,0.7)",
+    opacity: "0.9",
+    padding: "0.1rem 0.2rem 0.1rem 0.1rem",
+});
+
+const popUpText = css({
+    color: "white",
+});
+
+// Types
 type opsFilterProps = {
-    filterArgs: { category: string; values: (string | number)[] }[];
+    filterArgs: {
+        rarity: number[];
+        class: string[];
+        branch: string[];
+        faction: string[];
+    };
     classTree: { [key: string]: string[] };
     onClick: (condition: OpsFilterCondition) => void;
 };
+
+// Custom order for class sorting
 
 const customOrder: { [key: string]: number } = {
     Vanguard: 0,
@@ -26,6 +85,9 @@ const customOrder: { [key: string]: number } = {
 
 const OpsFilter = ({ filterArgs, classTree, onClick }: opsFilterProps) => {
     const [branchParent, setBranchParent] = useState<string | null>(null);
+    const rarities = filterArgs.rarity;
+    const classes = filterArgs.class;
+    const factions = filterArgs.faction;
 
     const handleClassClick = (value: string) => {
         setBranchParent(branchParent === value ? null : value);
@@ -44,69 +106,84 @@ const OpsFilter = ({ filterArgs, classTree, onClick }: opsFilterProps) => {
     };
 
     return (
-        <div className="filter__container">
-            {filterArgs
-                .filter((arg) => arg.category !== "branch")
-                .map((arg, index) => (
-                    <div
-                        key={index}
-                        className={`filter__${arg.category}__container`}
-                    >
-                        <h3 className="filter__title">
-                            {arg.category.toUpperCase()}
-                        </h3>
-                        <ul className={`filter__${arg.category}`}>
-                            {arg.values
-                                .sort((a, b) => customOrder[a] - customOrder[b])
-                                .map((value, index) => (
-                                    <li
-                                        key={index}
-                                        onClick={() =>
-                                            arg.category === "class"
-                                                ? handleClassClick(
-                                                      value.toString()
-                                                  )
-                                                : onClick({
-                                                      category: arg.category,
-                                                      value,
-                                                  })
-                                        }
-                                    >
-                                        <div className="filter__icon__container">
-                                            {(arg.category === "rarity" && (
-                                                <p className="filter__icon__text">
-                                                    ★{value}
-                                                </p>
-                                            )) || (
-                                                <>
-                                                    <Image
-                                                        className="filter__icon__image"
-                                                        src={getImageSource(
-                                                            arg.category,
-                                                            value.toString()
-                                                        )}
-                                                        height={30}
-                                                        width={30}
-                                                        alt={value.toString()}
-                                                    />
-                                                    <p className="filter__icon__desc">
-                                                        {value}
-                                                    </p>
-                                                </>
+        <>
+            <div className={filterSection}>
+                <h3 className={filterTitle}>Rarity</h3>
+            </div>
+            <div>
+                <ul className={rarityWrapper}>
+                    {rarities.map((rarity) => (
+                        <li key={rarity}>★{rarity}</li>
+                    ))}
+                </ul>
+            </div>
+
+            <div className={filterSection}>
+                <h3 className={filterTitle}>Class</h3>
+            </div>
+            <div>
+                <ul className={classWrapper}>
+                    {classes
+                        .sort((a, b) => customOrder[a] - customOrder[b])
+                        .map((classItem) => (
+                            <li key={classItem}>
+                                <HoverCard.Root>
+                                    <HoverCard.Trigger asChild>
+                                        <Image
+                                            src={getImageSource(
+                                                "class",
+                                                classItem
                                             )}
+                                            height={30}
+                                            width={30}
+                                            alt={classItem}
+                                        />
+                                    </HoverCard.Trigger>
+                                    <HoverCard.Portal>
+                                        <HoverCard.Content>
+                                            <BranchList
+                                                branches={classTree[classItem]}
+                                                onClick={onClick}
+                                            />
+                                        </HoverCard.Content>
+                                    </HoverCard.Portal>
+                                </HoverCard.Root>
+                            </li>
+                        ))}
+                </ul>
+            </div>
+
+            <div className={filterSection}>
+                <h3 className={filterTitle}>Faction</h3>
+            </div>
+            <div>
+                <ul className={factionWrapper}>
+                    {factions.map((faction) => (
+                        <li key={faction}>
+                            <HoverCard.Root>
+                                <HoverCard.Trigger asChild>
+                                    <Image
+                                        src={getImageSource("faction", faction)}
+                                        className={factionImage}
+                                        height={30}
+                                        width={30}
+                                        alt={faction}
+                                    />
+                                </HoverCard.Trigger>
+                                <HoverCard.Portal>
+                                    <HoverCard.Content>
+                                        <div className={popUpWrapper}>
+                                            <p className={popUpText}>
+                                                {faction}
+                                            </p>
                                         </div>
-                                        {arg.category === "class" &&
-                                            branchParent === value && (
-                                                <BranchList
-                                                    branches={classTree[value]}
-                                                    onClick={onClick}
-                                                />
-                                            )}
-                                    </li>
-                                ))}
-                        </ul>
-                    </div>
-                ))}
+                                    </HoverCard.Content>
+                                </HoverCard.Portal>
+                            </HoverCard.Root>
+                        </li>
+                    ))}
+                </ul>
+            </div>
 
             <h1
                 style={{ color: "red", textAlign: "center" }}
@@ -114,7 +191,7 @@ const OpsFilter = ({ filterArgs, classTree, onClick }: opsFilterProps) => {
             >
                 RESET
             </h1>
-        </div>
+        </>
     );
 };
 
