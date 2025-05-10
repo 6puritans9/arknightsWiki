82ccb87179a93a2e50f5776117fc2d055d697043
Lee Kyuhong <6puritans9@gmail.com>
2025-05-10 15:48:52 +0900

    refactor: improve BranchList component structure and styling

--- src/components/Filter/BranchList.tsx
+++ src/components/Filter/BranchList.tsx
@@ -2,40 +2,63 @@ import { OpsFilterCondition } from "@/lib/types";
 import Image from "next/image";
 import { branchMap } from "@/lib/constants/pathnameMap";
 import getS3Url from "@/lib/apiAws";
+import { flex } from "../../../styled-system/patterns";
+import { css } from "../../../styled-system/css";
 
 type BranchListProps = {
     branches: string[];
     onClick: (condition: OpsFilterCondition) => void;
 };
 
+const popUpWrapper = flex({
+    flexDirection: "column",
+    gap: "0.3rem",
+    backgroundColor: "rgba(0,0,0,0.7)",
+    opacity: "0.7",
+    padding: "0.1rem 0.2rem 0.1rem 0.1rem",
+});
+
+const branchWrapper = flex({
+    gap: "0.2rem",
+});
+
+const branchImage = css({
+    backgroundColor: "black",
+    height: "30px",
+    width: "30px",
+});
+
+const branchText = css({
+    color: "gray.200",
+});
+
 const BranchList = ({ branches, onClick }: BranchListProps) => {
     return (
-        <ul>
-            <div className="filter__branch__container">
-                {branches.map((branch, index) => (
-                    <li
-                        key={index}
-                        onClick={(e) => {
-                            e.stopPropagation();
-                            onClick({
-                                category: "branch",
-                                value: branch,
-                            });
-                        }}
-                    >
-                        <Image
-                            className="filter__icon__image"
-                            src={getS3Url(branchMap[branch])}
-                            height={30}
-                            width={30}
-                            alt="branch"
-                        />
-                        <p className="filter__icon__desc">{branch}</p>
-                    </li>
-                ))}
-            </div>
+        <ul className={popUpWrapper}>
+            {branches.map((branch, index) => (
+                <li
+                    key={index}
+                    className={branchWrapper}
+                    onClick={(e) => {
+                        e.stopPropagation();
+                        onClick({
+                            category: "branch",
+                            value: branch,
+                        });
+                    }}
+                >
+                    <Image
+                        className={branchImage}
+                        src={getS3Url(branchMap[branch])}
+                        height={30}
+                        width={30}
+                        alt="branch"
+                    />
+                    <span className={branchText}>{branch}</span>
+                </li>
+            ))}
         </ul>
     );
 };
 
-export { BranchList };
+export default BranchList;
