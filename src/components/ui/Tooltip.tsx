04d3604a19f771a8246fd14702cfa116e2d460a1
Lee Kyuhong <6puritans9@gmail.com>
2025-05-14 19:34:02 +0900

    refactor: update Popover and Tooltip components to use ComponentRef for better type safety

--- src/components/ui/Tooltip.tsx
+++ src/components/ui/Tooltip.tsx
@@ -5,13 +5,11 @@ import * as TooltipPrimitive from "@radix-ui/react-tooltip";
 import { css } from "../../../styled-system/css";
 
 const TooltipProvider = TooltipPrimitive.Provider;
-
 const Tooltip = TooltipPrimitive.Root;
-
 const TooltipTrigger = TooltipPrimitive.Trigger;
 
 const TooltipContent = React.forwardRef<
-    React.ElementRef<typeof TooltipPrimitive.Content>,
+    React.ComponentRef<typeof TooltipPrimitive.Content>,
     React.ComponentPropsWithoutRef<typeof TooltipPrimitive.Content>
 >(({ sideOffset = 4, ...props }, ref) => (
     <TooltipPrimitive.Content
@@ -21,16 +19,16 @@ const TooltipContent = React.forwardRef<
             zIndex: 50,
             overflow: "hidden",
             maxWidth: "350px",
+            backgroundColor: "transparent",
             padding: "0.5rem 0.75rem",
             borderRadius: "md",
             fontSize: "sm",
             lineHeight: "normal",
-            backgroundColor: "gray.800",
             color: "white",
             boxShadow: "md",
         })}
         {...props}
-    />
+    ></TooltipPrimitive.Content>
 ));
 TooltipContent.displayName = TooltipPrimitive.Content.displayName;
 
