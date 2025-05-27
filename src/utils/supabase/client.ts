f4a6b127d73403cfdbbc097c4b34d24d7aeeac78
Lee Kyuhong <6puritans9@gmail.com>
2025-05-28 00:56:08 +0900

    feat: add Supabase client setup for browser and server environments

---
+++ src/utils/supabase/client.ts
@@ -0,0 +1,15 @@
+import { createBrowserClient } from "@supabase/ssr";
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+
+if (!supabaseUrl) {
+    throw new Error("Missing Supabase URL");
+}
+if (!supabaseKey) {
+    throw new Error("Missing Supabase Key");
+}
+
+const supabase = createBrowserClient(supabaseUrl, supabaseKey);
+
+export default supabase;
