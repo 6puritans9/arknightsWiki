ad26f3cc3e14ca9d0c948c6f152996f6fbda39d4
Lee Kyuhong <6puritans9@gmail.com>
2025-06-27 18:05:13 +0900

    feat(apiMongo): add features

--- src/lib/apiMongo.ts
+++ src/lib/apiMongo.ts
@@ -1,5 +1,79 @@
 import { MongoClient } from "mongodb";
 
+//#region Type Definitions
+type attributesKeyFrameType = {
+    level: number;
+    data: {
+        maxHp: number;
+        atk: number;
+        def: number;
+        magicResistance: number;
+        cost: number;
+        blockCnt: number;
+        moveSpeed: number;
+        attackSpeed: number;
+        baseAttackTime: number;
+        respawnTime: number;
+        hpRecoveryPerSec: number;
+        spRecoveryPerSec: number;
+        maxDeployCount: number;
+        maxDeckStackCnt: number;
+        tauntLevel: number;
+        massLevel: number;
+        baseForceLevel: number;
+        stunImmune: boolean;
+        silenceImmune: boolean;
+        sleepImmune: boolean;
+        frozenImmune: boolean;
+        levitateImmune: boolean;
+        disarmedCombatImmune: boolean;
+        fearedImmune: boolean;
+    };
+};
+
+type PhaseType = {
+    characterPrefabKey: string;
+    rangeId: string;
+    maxLevel: number;
+    attributesKeyFrames: attributesKeyFrameType[];
+    evolveCost?: {
+        id: string;
+        count: number;
+        type: string;
+    }[];
+};
+
+type levelUpCostCondType = {
+    unlockCond: {
+        phase: string;
+        level: number;
+    };
+    lvlUpTime: number;
+    levelUpCost: {
+        id: string;
+        count: number;
+        type: string;
+    }[];
+};
+
+type skillType = {
+    skillId: string;
+    levelUpCostCond: levelUpCostCondType[];
+};
+
+export type ThumbnailOperatorType = {
+    _id: string;
+    name: string;
+    appellation: string | null;
+    rarity: number;
+    profession: string;
+    subProfessionId: string;
+    nationId: string;
+    teamId?: string;
+    groupId?: string;
+    displayNumber: string;
+};
+
 export type OperatorType = {
     _id: string;
     isNotObtainable: boolean; // determine npc characters
@@ -17,9 +91,16 @@ export type OperatorType = {
     nationId: string;
     groupId?: string;
     teamId?: string;
+    phases: PhaseType[];
+    skills: skillType[];
+    // talents: 1;
+    // potentialRanks: 1;
+    // favorKeyFrames: 1;
+    // allSkillLvlup: 1;
 };
+//#endregion Type Definitions
 
-let client: MongoClient;
+//#region Database mapping
 const LANGUAGE_DB_MAP: { [key: string]: string } = {
     en: "data_en",
     cn: "data_cn",
@@ -27,7 +108,30 @@ const LANGUAGE_DB_MAP: { [key: string]: string } = {
     kr: "data_kr",
     tw: "data_tw",
 };
+const rarityMap: { [key: string]: number } = {
+    TIER_1: 1,
+    TIER_2: 2,
+    TIER_3: 3,
+    TIER_4: 4,
+    TIER_5: 5,
+    TIER_6: 6,
+};
+//#endregion Database mapping
 
+// MongoDB client instance
+let client: MongoClient;
+
+// Helper function to clean string fields
+const cleanStringField = (value: string): string | null => {
+    if (!value) {
+        return null;
+    }
+
+    const trimmed = value.trim();
+    return trimmed === "" ? null : trimmed;
+};
+
+// Main Functions
 const getClient = async () => {
     if (!client) {
         client = new MongoClient(process.env.MONGODB_URI as string);
@@ -37,20 +141,65 @@ const getClient = async () => {
     return client;
 };
 
+const fetchAllOperators = async (
+    lang: string = "en"
+): Promise<ThumbnailOperatorType[]> => {
+    try {
+        const mongoClient = await getClient();
+        const dbName = LANGUAGE_DB_MAP[lang];
+        const db = mongoClient.db(dbName);
+        const collection = db.collection("character_table");
+
+        let query = collection.find(
+            {
+                $and: [
+                    { isNotObtainable: false },
+                    { maxPotentialLevel: { $gt: 0 } },
+                ],
+            },
+            {
+                projection: {
+                    _id: 1,
+                    name: 1,
+                    appellation: 1,
+                    rarity: 1,
+                    profession: 1,
+                    subProfessionId: 1,
+                    nationId: 1,
+                    teamId: 1,
+                    groupId: 1,
+                    displayNumber: 1,
+                },
+            }
+        );
+
+        query = query.sort({ rarity: -1, displayNumber: -1 });
+        const operators = await query.toArray();
+
+        return operators.map((op) => ({
+            _id: op._id.toString(),
+            name: op.name,
+            appellation: op.appellation || null,
+            rarity: rarityMap[op.rarity],
+            profession: op.profession,
+            subProfessionId: op.subProfessionId,
+            nationId: op.nationId,
+            teamId: op.teamId || null,
+            groupId: op.groupId || null,
+            displayNumber: op.displayNumber,
+        }));
+    } catch (error) {
+        console.error("Error fetching operators:", error);
+        throw new Error("Failed to fetch operators");
+    }
+};
+
 const fetchSingleEntity = async (
     name: string,
     collectionName: string,
     lang: string = "en"
 ) => {
     const decodedName = decodeURIComponent(name);
-    const rarityMap: { [key: string]: number } = {
-        TIER_1: 1,
-        TIER_2: 2,
-        TIER_3: 3,
-        TIER_4: 4,
-        TIER_5: 5,
-        TIER_6: 6,
-    };
 
     try {
         const mongoClient = await getClient();
@@ -77,6 +226,12 @@ const fetchSingleEntity = async (
                     nationId: 1,
                     groupId: 1,
                     teamId: 1,
+                    phases: 1,
+                    skills: 1,
+                    // talents: 1,
+                    // potentialRanks: 1,
+                    // favorKeyFrames: 1,
+                    // allSkillLvlup: 1,
                 },
             }
         );
@@ -87,14 +242,6 @@ const fetchSingleEntity = async (
             );
             return null;
         }
-
-        const cleanStringField = (value: string): string | null => {
-            let trimmed = null;
-            if (value) {
-                trimmed = value.trim();
-            }
-            return trimmed || null;
-        };
 
         return {
             _id: entity._id.toString(),
@@ -112,6 +259,12 @@ const fetchSingleEntity = async (
             nationId: entity.nationId,
             groupId: cleanStringField(entity.groupId),
             teamId: cleanStringField(entity.teamId),
+            phases: entity.phases,
+            skills: entity.skills,
+            // talents: 1,
+            // potentialRanks: 1,
+            // favorKeyFrames: 1,
+            // allSkillLvlup: 1,
         } as OperatorType;
     } catch (error) {
         console.error("Error connecting to MongoDB:", error);
@@ -119,4 +272,4 @@ const fetchSingleEntity = async (
     }
 };
 
-export { fetchSingleEntity };
+export { fetchSingleEntity, fetchAllOperators };
