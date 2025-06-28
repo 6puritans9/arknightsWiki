635379fa1752f7aaec9955a4dbafb837911b28cd
Lee Kyuhong <6puritans9@gmail.com>
2025-06-29 05:05:00 +0900

    feat: add i18n text dictionary

---
+++ src/lib/dictionary.ts
@@ -0,0 +1,26 @@
+export const appPageText = {
+    event: {
+        ongoing: {
+            en: "Ongoing",
+            ko: "진행중",
+            ja: "開催中",
+            cn: "进行中",
+            tw: "進行中",
+        },
+        upcoming: {
+            en: "Upcoming",
+            ko: "예정",
+            ja: "予定",
+            cn: "即将到来",
+            tw: "即將到來",
+        },
+    },
+};
+
+export const footerLogoText = {
+    en: "RHODES ISLAND",
+    ko: "로도스 아일랜드",
+    ja: "ロドスアイランド",
+    cn: "罗德岛",
+    tw: "羅德島",
+};
