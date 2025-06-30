9a879880b4bab1cfbff5470335686ebfc8921eb6
Lee Kyuhong <6puritans9@gmail.com>
2025-06-30 19:07:46 +0900

    feat: refactor InfrastructureClient and InfraPage components

--- src/app/infrastructure/clientPage.tsx
+++ src/app/infrastructure/clientPage.tsx
@@ -1,224 +1,144 @@
 "use client";
 
-import { useState, useReducer } from "react";
-import {
-    BaseFilterCondition,
-    BaseFilterState,
-    BaseFilterAction,
-    BaseRelationsValue,
-} from "@/lib/types";
-import { QueryBaseSkills } from "@/lib/types";
-import { InfraFilter } from "@/components/Filter/InfraFilter";
-import InfraThumbnail from "@/components/InfraThumbnail";
+import { useState, useMemo } from "react";
+import { usePagination } from "@/hooks/usePagination";
+import { css } from "../../../styled-system/css";
+import { BuildingBuffType, BuildingCharType } from "@/lib/apiMongo";
+import InfraCard from "@/components/infra/InfraCard";
 
-type InfrastructureClientProps = {
-    initialData: QueryBaseSkills;
-};
-
-const initialFilterState = {
-    selfName: null,
-    facility: null,
-    effects: null,
-    relations: null,
-};
-
-const baseReducer = (
-    state: BaseFilterState,
-    action: BaseFilterAction
-): BaseFilterState => {
-    switch (action.type) {
-        case "SET_FACILITY":
-            return {
-                ...state,
-                facility: action.payload,
-                selfName: action.selfName,
-            };
-        case "SET_EFFECTS":
-            return {
-                ...initialFilterState,
-                effects: action.payload,
-                selfName: action.selfName,
-            };
-        case "SET_RELATIONS":
-            return {
-                ...initialFilterState,
-                relations: action.payload,
-                selfName: action.selfName,
-            };
-        case "RESET":
-            return initialFilterState;
-    }
-};
-
-const InfrastructureClient = ({ initialData }: InfrastructureClientProps) => {
-    const [data, setData] = useState<QueryBaseSkills>(initialData);
-    const [filter, dispatch] = useReducer(baseReducer, initialFilterState);
-
-    if (initialData instanceof Error) {
-        const error = initialData.message;
-        return <div>{error}</div>;
-    }
-
-    const filterHandler = (condition: BaseFilterCondition) => {
-        switch (condition.category) {
-            case "facility":
-                dispatch({
-                    type: "SET_FACILITY",
-                    selfName: condition.selfName ?? null,
-                    payload: condition.value as string,
-                });
-                break;
-            case "effects":
-                dispatch({
-                    type: "SET_EFFECTS",
-                    selfName: condition.selfName ?? null,
-                    payload: condition.value as string,
-                });
-                break;
-            case "relations":
-                dispatch({
-                    selfName: condition.selfName ?? null,
-                    type: "SET_RELATIONS",
-                    payload: condition.value as BaseRelationsValue,
-                });
-                break;
-            default:
-                dispatch({ type: "RESET" });
-        }
+type InfraClientPageProps = {
+    initialData: {
+        chars: BuildingCharType[];
+        buffs: BuildingBuffType[];
+        nameToIdMap: { [key: string]: string };
     };
+};
 
-    const facilitySet = new Set(
-        data.flatMap((operator) =>
-            operator.operator_base.flatMap((row) => {
-                const baseArray = Array.isArray(row.base)
-                    ? row.base
-                    : [row.base];
+const container = css({
+    display: "grid",
+    gridTemplateColumns: {
+        base: "1fr",
+        sm: "repeat(2, 1fr)",
+        md: "repeat(4, 1fr)",
+    },
+    gap: "1.5rem",
+    width: "100%",
+    justifyItems: "center",
+    padding: "1rem 0",
+});
 
-                return baseArray.map((baseItem) => baseItem.facility);
-            })
-        )
-    );
+// const initialFilterState = {
+//     selfName: null,
+//     facility: null,
+//     effects: null,
+//     relations: null,
+// };
 
-    const effectSet = new Set(
-        data.flatMap((operator) =>
-            operator.operator_base.flatMap((row) => {
-                const baseArray = Array.isArray(row.base)
-                    ? row.base
-                    : [row.base];
+// const baseReducer = (
+//     state: BaseFilterState,
+//     action: BaseFilterAction
+// ): BaseFilterState => {
+//     switch (action.type) {
+//         case "SET_FACILITY":
+//             return {
+//                 ...state,
+//                 facility: action.payload,
+//                 selfName: action.selfName,
+//             };
+//         case "SET_EFFECTS":
+//             return {
+//                 ...initialFilterState,
+//                 effects: action.payload,
+//                 selfName: action.selfName,
+//             };
+//         case "SET_RELATIONS":
+//             return {
+//                 ...initialFilterState,
+//                 relations: action.payload,
+//                 selfName: action.selfName,
+//             };
+//         case "RESET":
+//             return initialFilterState;
+//     }
+// };
 
-                return baseArray.flatMap((baseItem) => baseItem.effects);
-            })
-        )
-    );
-
-    const filterArgs = [
-        { category: "facility", values: Array.from(facilitySet) },
-        { category: "effects", values: Array.from(effectSet) },
-    ];
-
-    const filteredData = data.filter((operator) => {
-        const skill = operator.operator_base.flatMap((row) => {
-            const baseArray = Array.isArray(row.base) ? row.base : [row.base];
-            return baseArray;
+const InfraClientPage = ({
+    initialData: { chars, buffs, nameToIdMap: pathMap },
+}: InfraClientPageProps) => {
+    const buffMap = useMemo(() => {
+        const map: { [key: string]: BuildingBuffType } = {};
+        buffs.forEach((buff) => {
+            map[buff.buffId] = buff;
         });
-        const effects = skill.flatMap((row) => row.effects);
-        const facility = skill.map((row) => row.facility);
 
-        if (filter.selfName === operator.name) {
-            return true;
-        }
-        if (!filter.facility && !filter.effects && !filter.relations) {
-            return true;
-        } else if (filter.facility && facility.includes(filter.facility)) {
-            return true;
-        } else if (filter.effects && effects.includes(filter.effects)) {
-            return true;
-        } else if (filter.relations) {
-            return (
-                (filter.relations.r_effects &&
-                    filter.relations.r_effects.some((r_effect: string) =>
-                        effects.includes(r_effect)
-                    )) ||
-                (filter.relations.r_ops &&
-                    filter.relations.r_ops.some(
-                        (r_op: string) => operator.name === r_op
-                    )) ||
-                (filter.relations.r_faction &&
-                    operator.faction === filter.relations.r_faction) ||
-                (filter.relations.r_faction &&
-                    operator.sub_factions &&
-                    operator.sub_factions.includes(filter.relations.r_faction))
-            );
-        }
-        return false;
-    });
+        return map;
+    }, [buffs]);
 
+    const { itemsToShow, hasMore, loaderRef } = usePagination(chars, 20);
+
+    //     return (
+    //         <>
+    //             {chars.map((char) => {
+    //                 return (
+    //                     <InfraCard
+    //                         key={char.charId}
+    //                         char={char}
+    //                         buffMap={buffMap}
+    //                     />
+    //                 );
+    //             })}
+    //         </>
+    //     );
+    // };
     return (
         <>
-            <div className="flex">
-                <InfraFilter filterArgs={filterArgs} onClick={filterHandler} />
-            </div>
-            <section className="infra__container">
-                {filteredData.map((operator) => (
-                    <div className="infra__icon__container" key={operator.id}>
-                        <InfraThumbnail operator={operator} />
-                        {operator.operator_base.map((row, index) => {
-                            const baseArray = Array.isArray(row.base)
-                                ? row.base
-                                : [row.base];
-
-                            return (
-                                <article
-                                    key={row.base_id}
-                                    className="infra__icon__content"
-                                    onClick={() =>
-                                        filterHandler({
-                                            category: "relations",
-                                            selfName: operator.name,
-                                            value: {
-                                                r_effects: baseArray.flatMap(
-                                                    (base) =>
-                                                        base.related_effects ||
-                                                        []
-                                                ),
-                                                r_ops: baseArray.flatMap(
-                                                    (base) =>
-                                                        base.related_ops || []
-                                                ),
-                                                r_faction:
-                                                    baseArray[0]
-                                                        ?.related_faction ||
-                                                    null,
-                                                r_race:
-                                                    baseArray[0]
-                                                        ?.related_race || null,
-                                                r_facilities: baseArray.flatMap(
-                                                    (base) =>
-                                                        base.related_facilities ||
-                                                        []
-                                                ),
-                                            },
-                                        })
-                                    }
-                                >
-                                    <h4 className="infra__icon__content-name">{`${
-                                        index + 1
-                                    } ${baseArray
-                                        .map((base) => base.name)
-                                        .join(", ")}`}</h4>
-                                    <p className="infra__icon__content-desc">
-                                        {baseArray
-                                            .map((base) => base.description)
-                                            .join(", ")}
-                                    </p>
-                                </article>
-                            );
-                        })}
-                    </div>
+            <section className={container}>
+                {itemsToShow.map((char) => (
+                    <InfraCard
+                        key={char.charId}
+                        char={char}
+                        buffMap={buffMap}
+                    />
                 ))}
             </section>
+            {hasMore && (
+                <div
+                    ref={loaderRef}
+                    className={css({
+                        padding: "2rem",
+                        textAlign: "center",
+                        width: "100%",
+                    })}
+                >
+                    <LoadingSpinner />
+                </div>
+            )}
         </>
     );
 };
 
-export default InfrastructureClient;
+function LoadingSpinner() {
+    return (
+        <div
+            className={css({
+                display: "inline-block",
+                width: "2rem",
+                height: "2rem",
+                border: "4px solid rgba(0,0,0,0.1)",
+                borderRadius: "50%",
+                borderLeftColor: "#09f",
+                animation: "spin 1s linear infinite",
+            })}
+        >
+            <style jsx>{`
+                @keyframes spin {
+                    to {
+                        transform: rotate(360deg);
+                    }
+                }
+            `}</style>
+        </div>
+    );
+}
+
+export default InfraClientPage;
--- src/app/infrastructure/page.tsx
+++ src/app/infrastructure/page.tsx
@@ -1,10 +1,110 @@
-import { getBaseSkills } from "@/lib/apiBase";
-import InfrastructureClient from "./clientPage";
+import { flex, grid } from "../../../styled-system/patterns";
+import { fetchAllBuildingData } from "@/lib/apiMongo";
+import InfraClientPage from "./clientPage";
+import { RoomType, BuildingBuffType } from "@/lib/apiMongo";
 
-const Infrastructure = async () => {
-    const data = await getBaseSkills();
+const wrapper = flex({
+    flexDirection: "column",
+    alignItems: "center",
+    width: "100%",
+    maxWidth: "1200px",
+    margin: "0 auto",
+});
 
-    return <InfrastructureClient initialData={data} />;
+const filterContainer = grid({
+    gridTemplateColumns: "1fr 9fr",
+    gridTemplateAreas: `
+    "rarity-label rarity-content"
+    "class-label class-content"
+    "faction-label faction-content"
+  `,
+    gap: "1rem",
+    justifyItems: "flex-start",
+    marginBottom: "1rem",
+    width: "100%",
+});
+
+const cardContainer = grid({
+    gridTemplateColumns: {
+        base: "1fr",
+        sm: "repeat(2, 1fr)",
+        lg: "repeat(4, 1fr)",
+    },
+    gap: "1rem",
+    justifyItems: "center",
+    width: "100%",
+    padding: "1rem 0",
+});
+
+const InfraPage = async () => {
+    const { chars, buffs, nameToIdMap } = await fetchAllBuildingData();
+
+    const roomBuffTree = ((buffs: BuildingBuffType[]) => {
+        const _roomBuffTree: { [key in RoomType]?: BuildingBuffType[] } = {};
+
+        buffs.forEach((buff) => {
+            const roomType = buff.roomType as RoomType;
+
+            if (!Array.isArray(_roomBuffTree[roomType])) {
+                _roomBuffTree[roomType] = [];
+            }
+            _roomBuffTree[roomType].push(buff);
+        });
+
+        return _roomBuffTree;
+    })(buffs);
+
+    // const filterArgs = {
+    //     rooms: Object.keys(roomBuffTree),
+    //     buffs: Object.values(roomBuffTree).flat(),
+    // };
+
+    return (
+        <div className={wrapper}>
+            {/* Hydration */}
+            <section className={filterContainer}>
+                <div>
+                    {/* {Object.entries(roomBuffTree).map(([roomType, buffs]) => (
+                        <div key={roomType}>
+                            <h3>{roomType}</h3>
+                        </div>
+                    ))} */}
+                    <p>FILTER</p>
+                </div>
+                {/* <div>
+                    {filterArgs.buffs.map((buff) => (
+                        <div
+                            key={buff.buffId}
+                            style={{ marginBottom: "0.5rem" }}
+                        >
+                            <p>{`Name: ${buff.buffName}`}</p>
+                            <p>{`ID: ${buff.buffId}`}</p>
+                            <p>{`Category: ${buff.buffCategory}`}</p>
+                            <p>{`Description: ${buff.description}`}</p>
+                        </div>
+                    ))}
+                </div> */}
+                {/* <ClientSideFilterWrapper
+                    filterArgs={filterArgs}
+                    classTree={classTree}
+                    factionTree={factionTree}
+                    initialData={data}
+                /> */}
+            </section>
+
+            {/* Server-rendered operators */}
+            <section id="initial-operators" className={cardContainer}>
+                {/* {initialVisibleOperators.map((operator, index) => (
+                    <OpsThumbnail
+                        key={operator._id}
+                        operator={operator}
+                        priority={index < 5}
+                    />
+                ))} */}
+                <InfraClientPage initialData={{ chars, buffs, nameToIdMap }} />
+            </section>
+        </div>
+    );
 };
 
-export default Infrastructure;
+export default InfraPage;
