1ed6f88bdc4fd26898cc3d8d9df9aba58abd5d1f
Lee Kyuhong <6puritans9@gmail.com>
2025-07-03 01:55:21 +0900

    feat: refactor InfraFilter to use Accordion component

--- src/components/Filter/infra/InfraFilter.tsx
+++ src/components/Filter/infra/InfraFilter.tsx
@@ -2,13 +2,24 @@ import { BuffsObjectType } from "@/lib/apiMongo";
 // import parseDynamicKeywords from "@/utils/keywordParser";
 import { flex } from "../../../../styled-system/patterns";
 import useInfraStore from "@/stores/infraStore";
+import { Accordion } from "radix-ui";
+import {
+    AccordionTrigger,
+    AccordionContent,
+    accordionRootStyle,
+    accordionItemStyle,
+    accordionContentStyle,
+} from "@/components/ui/Accordion";
+import { css } from "../../../../styled-system/css";
 
 type FilterProps = {
     buffs: BuffsObjectType;
     roomEffectTree: { [key: string]: string[] };
 };
 
-const container = flex({});
+const trigger = css({
+    backgroundColor: "blue",
+});
 const wrapper = flex({});
 
 // Handles dynamic keyword edge cases for filtering
@@ -26,44 +37,62 @@ const InfraFilter = ({ buffs, roomEffectTree }: FilterProps) => {
     };
 
     return (
-        <div className={container}>
-            <div className={wrapper}>
-                {Object.keys(roomEffectTree).map((room) => (
-                    <div key={room}>
-                        <p onClick={() => filterHandler("rooms", room)}>
-                            {room}
-                        </p>
-                        <div>
-                            {Object.values(roomEffectTree[room]).map(
-                                (effect) => (
-                                    <p
-                                        key={effect}
-                                        style={{
-                                            color:
-                                                buffs[effect]?.buffColor ??
-                                                "blue",
-                                            fontSize: "0.8rem",
-                                        }}
-                                        onClick={() =>
-                                            filterHandler("effects", effect)
-                                        }
-                                    >
-                                        {effect}
-                                    </p>
-                                )
-                            )}
-                        </div>
+        <Accordion.Root className={accordionRootStyle} type="multiple">
+            <Accordion.Item className={accordionItemStyle} value="desc">
+                <AccordionTrigger>About this page</AccordionTrigger>
+                <AccordionContent>
+                    <p className={accordionContentStyle}>
+                        You can click filter or you can click highlited related
+                        attributes in the card
+                    </p>
+                </AccordionContent>
+            </Accordion.Item>
+            <Accordion.Item className={accordionItemStyle} value="filter">
+                <AccordionTrigger>Open Filter</AccordionTrigger>
+                <AccordionContent>
+                    <div className={wrapper}>
+                        {Object.keys(roomEffectTree).map((room) => (
+                            <div key={room}>
+                                <p onClick={() => filterHandler("rooms", room)}>
+                                    {room}
+                                </p>
+                                <div>
+                                    {Object.values(roomEffectTree[room]).map(
+                                        (effect) => (
+                                            <p
+                                                key={effect}
+                                                style={{
+                                                    color:
+                                                        buffs[effect]
+                                                            ?.buffColor ??
+                                                        "blue",
+                                                    fontSize: "0.8rem",
+                                                }}
+                                                onClick={() =>
+                                                    filterHandler(
+                                                        "effects",
+                                                        effect
+                                                    )
+                                                }
+                                            >
+                                                {effect}
+                                            </p>
+                                        )
+                                    )}
+                                </div>
+                            </div>
+                        ))}
                     </div>
-                ))}
-            </div>
 
-            <h1
-                style={{ color: "red", textAlign: "center" }}
-                onClick={() => resetFilters()}
-            >
-                RESET
-            </h1>
-        </div>
+                    <h1
+                        style={{ color: "red", textAlign: "center" }}
+                        onClick={() => resetFilters()}
+                    >
+                        RESET
+                    </h1>
+                </AccordionContent>
+            </Accordion.Item>
+        </Accordion.Root>
     );
 };
 
