4dc1fe862007d90cd29fa10a2f97ee879cf2d4d3
Lee Kyuhong <6puritans9@gmail.com>
2025-07-03 18:55:04 +0900

    feat: add getRoomImage function

--- src/lib/apiAws.ts
+++ src/lib/apiAws.ts
@@ -1,3 +1,5 @@
+import { RoomType } from "./apiMongo";
+
 const getS3Url = (path: string) => {
     const baseUrl = process.env.NEXT_PUBLIC_S3_URL;
 
@@ -25,7 +27,23 @@ const getFactionImage = (id: string): string =>
     `https://arknights-wiki-assets.s3.ap-northeast-2.amazonaws.com/public/op_misc/camplogo/logo_${id.toLocaleLowerCase()}.png`;
 
 const getEliteImage = (phase: string) =>
-    `https://arknights-wiki-assets.s3.ap-northeast-2.amazonaws.com/dynamicassets/arts/elite_hub/elite_${phase}_card.png`;
+    `https://arknights-wiki-assets.s3.ap-northeast-2.amazonaws.com/dynamicassets/arts/elite_hub/elite_${phase}.png`;
+
+const getRoomImage = (room: RoomType): string => {
+    if (room == "CONTROL" || room == "TRAINING" || room == "WORKSHOP") {
+        return `https://arknights-wiki-assets.s3.ap-northeast-2.amazonaws.com/dynamicassets/arts/building/architect/room_icon_sprite_hub/icon_${room.toLowerCase()}.png`;
+    }
+
+    if (room == "DORMITORY") {
+        return `https://arknights-wiki-assets.s3.ap-northeast-2.amazonaws.com/dynamicassets/arts/building/architect/room_icon_sprite_hub/icon_title_dorm.png`;
+    }
+
+    if (room == "MANUFACTURE") {
+        return `https://arknights-wiki-assets.s3.ap-northeast-2.amazonaws.com/dynamicassets/arts/building/architect/room_icon_sprite_hub/icon_title_manufact.png`;
+    }
+
+    return `https://arknights-wiki-assets.s3.ap-northeast-2.amazonaws.com/dynamicassets/arts/building/architect/room_icon_sprite_hub/icon_title_${room.toLowerCase()}.png`;
+};
 
 export default getS3Url;
 export {
@@ -34,4 +52,5 @@ export {
     getsubProfessionIdImage,
     getFactionImage,
     getEliteImage,
+    getRoomImage,
 };
