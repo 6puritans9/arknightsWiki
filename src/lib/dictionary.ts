c301424ee6603623045a7cd9b9eb8ebf2efe9667
Lee Kyuhong <6puritans9@gmail.com>
2025-07-03 18:55:41 +0900

    feat: add greeting messages and "PRIVATE" room type to dictionary

--- src/lib/dictionary.ts
+++ src/lib/dictionary.ts
@@ -1,3 +1,15 @@
+import { RoomType } from "./apiMongo";
+
+type Locales = "en" | "ko" | "ja" | "cn" | "tw";
+
+export const greetMsg: { [key in Locales]: string } = {
+    en: "Hello, doctor ${name}",
+    ko: "안녕하세요 ${name} 박사님",
+    cn: "",
+    ja: "",
+    tw: "",
+};
+
 export const appPageText = {
     event: {
         ongoing: {
@@ -29,3 +41,47 @@ export const notFoundText = {
     en: "Page Not Found",
     ko: "페이지를 찾을 수 없습니다",
 };
+
+export const roomNameMap: { [key in RoomType]: { [key in Locales]: string } } =
+    {
+        CONTROL: {
+            en: "Control Center",
+            ko: "제어 센터",
+            cn: "",
+            ja: "",
+            tw: "",
+        },
+        DORMITORY: { en: "Dormitory", ko: "숙소", cn: "", ja: "", tw: "" },
+        HIRE: { en: "HR Office", ko: "사무실", cn: "", ja: "", tw: "" },
+        MANUFACTURE: { en: "Factory", ko: "제조소", cn: "", ja: "", tw: "" },
+        MEETING: {
+            en: "Reception Room",
+            ko: "응접실",
+            cn: "",
+            ja: "",
+            tw: "",
+        },
+        POWER: { en: "Power Plant", ko: "발전소", cn: "", ja: "", tw: "" },
+        TRADING: {
+            en: "Trading Post",
+            ko: "무역소",
+            cn: "",
+            ja: "",
+            tw: "",
+        },
+        TRAINING: {
+            en: "raining Room",
+            ko: "훈련실",
+            cn: "",
+            ja: "",
+            tw: "",
+        },
+        WORKSHOP: { en: "Workshop", ko: "가공소", cn: "", ja: "", tw: "" },
+        PRIVATE: {
+            en: "Activity Room",
+            ko: "",
+            cn: "",
+            ja: "",
+            tw: "",
+        },
+    };
