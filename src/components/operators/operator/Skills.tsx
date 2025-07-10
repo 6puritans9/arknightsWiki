53672780a1850344aaa301e17d9263b536ec59c2
Lee Kyuhong <6puritans9@gmail.com>
2025-07-11 01:29:48 +0900

    feat: refactor Skills component to use TextProcessor

--- src/components/operators/operator/Skills.tsx
+++ src/components/operators/operator/Skills.tsx
@@ -1,16 +1,16 @@
 import Image from "next/image";
 import { useState, useMemo } from "react";
 import { getSkillImage } from "@/api/apiAws";
-import { TabProps } from "./OperatorTabs";
+import { SingleOpType } from "@/api/apiMongo";
 import { flex } from "$/styled-system/patterns";
 import { css } from "$/styled-system/css";
-import {
-    filledRichText,
-    newlineRichText,
-    parseRichText,
-    extractSkillRange,
-} from "@/components/text/TextConverter";
+import TextProcessor from "@/components/text/TextProcessor";
+import { extractSkillRange } from "@/components/text/TextProcessor";
 import RangeTable from "./RangeTable";
+
+export type SkillsProps = {
+    operator: SingleOpType;
+};
 
 //#region Styles
 const selectorWrapper = flex({
@@ -40,14 +40,16 @@ const levelSelectorWrapper = flex({
     gap: {
         base: "0.5rem",
     },
+    cursor: "pointer",
 });
 
 const selected = css({
     color: "red",
+    fontWeight: "bold",
 });
 //#endregion
 
-const Skills = ({ operator }: TabProps) => {
+const Skills = ({ operator }: SkillsProps) => {
     const [activeSkillId, setActiveSkillId] = useState<string>(
         operator.skills[0].skillId
     );
@@ -74,14 +76,14 @@ const Skills = ({ operator }: TabProps) => {
     const parsedDesc = useMemo(() => {
         if (!currentSkill) return [];
 
-        return filledRichText(
-            newlineRichText(
-                parseRichText(currentSkill.levels[activeLevel].description)
-            ),
-            operator,
-            currentSkillIndex,
-            activeLevel
-        );
+        const rawDescription = currentSkill.levels[activeLevel].description;
+
+        return TextProcessor.from(rawDescription)
+            .parseRichText()
+            .parseNewlines()
+            .fillValues(operator, currentSkillIndex, activeLevel)
+            .parsePopovers()
+            .getResult();
     }, [currentSkill, currentSkillIndex, activeLevel, operator]);
 
     const skillRange = useMemo(() => {
@@ -91,84 +93,82 @@ const Skills = ({ operator }: TabProps) => {
 
     const handleSkillClick = (skillId: string, idx: number) => {
         setActiveSkillId(skillId);
-        setActiveLevel(operator.skillDetails[idx]?.levels.length - 1 || 9);
+        setActiveLevel(operator.skillDetails[idx]?.levels.length - 1);
     };
 
     return (
         <>
-            <div>
-                <div className={selectorWrapper}>
-                    {orderedSkillTable.map((skill, idx) => {
-                        const id = skill.skillId;
-                        const iconId = skill.iconId || id;
-                        const skillName = skill.levels[0].name;
+            <div className={selectorWrapper}>
+                {orderedSkillTable.map((skill, idx) => {
+                    const id = skill.skillId;
+                    const iconId = skill.iconId || id;
+                    const skillName = skill.levels[0].name;
 
-                        return (
-                            <figure
-                                key={idx}
-                                onClick={() => handleSkillClick(id, idx)}
-                                className={selectorStyle}
-                            >
-                                <Image
-                                    src={getSkillImage(iconId)}
-                                    className={skillImageStyle}
-                                    width={60}
-                                    height={60}
-                                    alt="skillImg"
-                                />
-                                <figcaption>
-                                    <section>{skillName}</section>
-                                </figcaption>
-                            </figure>
-                        );
-                    })}
-                </div>
-                {currentSkill && (
-                    <article>
-                        <div className={levelSelectorWrapper}>
-                            {currentSkill.levels.map((level, idx) => (
-                                <span
-                                    key={idx}
-                                    className={
-                                        activeLevel === idx ? selected : ""
-                                    }
-                                    onClick={() => setActiveLevel(idx)}
-                                >
-                                    {idx === 7
-                                        ? "M1"
-                                        : idx === 8
-                                          ? "M2"
-                                          : idx === 9
-                                            ? "M3"
-                                            : idx + 1}
-                                </span>
-                            ))}
-                        </div>
-                        <section>
-                            <p>{parsedDesc}</p>
-                            {skillRange && (
-                                <>
-                                    <p style={{ textAlign: "center" }}>
-                                        projectile buff range
-                                    </p>
-                                    <RangeTable
-                                        range={skillRange}
-                                        projectileSelf={true}
-                                    />
-                                </>
-                            )}
-
-                            {currentSkill.levels[activeLevel].rangeId && (
-                                <RangeTable
-                                    range={
-                                        currentSkill.levels[activeLevel].rangeId
-                                    }
-                                />
-                            )}
-                        </section>
-                    </article>
-                )}
+                    return (
+                        <figure
+                            key={idx}
+                            onClick={() => handleSkillClick(id, idx)}
+                            className={selectorStyle}
+                        >
+                            <Image
+                                src={getSkillImage(iconId)}
+                                className={skillImageStyle}
+                                width={60}
+                                height={60}
+                                alt="skillImg"
+                            />
+                            <figcaption>
+                                <section>{skillName}</section>
+                            </figcaption>
+                        </figure>
+                    );
+                })}
             </div>
+            {currentSkill && (
+                <article>
+                    <div className={levelSelectorWrapper}>
+                        {currentSkill.levels.map((level, idx) => (
+                            <span
+                                key={idx}
+                                className={activeLevel === idx ? selected : ""}
+                                onClick={() => setActiveLevel(idx)}
+                            >
+                                {idx === 7
+                                    ? "M1"
+                                    : idx === 8
+                                      ? "M2"
+                                      : idx === 9
+                                        ? "M3"
+                                        : idx + 1}
+                            </span>
+                        ))}
+                    </div>
+                    <section>
+                        <p>{parsedDesc}</p>
+                        {skillRange && (
+                            <>
+                                <p
+                                    style={{
+                                        textAlign: "center",
+                                    }}
+                                >
+                                    projectile buff range
+                                </p>
+                                <RangeTable
+                                    range={skillRange}
+                                    projectileSelf={true}
+                                />
+                            </>
+                        )}
+
+                        {currentSkill.levels[activeLevel].rangeId && (
+                            <RangeTable
+                                range={currentSkill.levels[activeLevel].rangeId}
+                            />
+                        )}
+                    </section>
+                </article>
+            )}
         </>
     );
 };
