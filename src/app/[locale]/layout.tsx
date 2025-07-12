63400126d8c31ba608f91c329e0610ed5ae78890
Lee Kyuhong <6puritans9@gmail.com>
2025-07-12 14:15:49 +0900

    refactor: refactor layout structure to support locale-specific rendering and remove legacy layout file

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,22 +1,66 @@
+import "@/app/globals.css";
+import Paperlogy from "../styles/fonts";
+import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { TouchProvider } from "@/components/utils/TouchProvider";
+import StoreInitializer from "@/components/utils/StoreInitializer";
+// import LocaleInitializer from "@/components/utils/LocaleInitializer";
 import Header from "@/components/layout/Header";
-import Footer from "@/components/layout/Footer";
+import { flex } from "$/styled-system/patterns";
 
-const LocaleLayout = async ({
-    children,
-    params,
-}: {
-    children: React.ReactNode;
-    params: Promise<{ locale: string }>;
-}) => {
-    const { locale } = await params;
-
-    return (
-        <>
-            <Header locale={locale} />
-            {children}
-            <Footer locale={locale} />
-        </>
-    );
+export const metadata: Metadata = {
+    title: "Rhodes Island",
+    description: "Arknights Wiki",
 };
 
-export default LocaleLayout;
+type LocaleLayoutProps = {
+    children: React.ReactNode;
+    params: Promise<{ locale: string }>;
+};
+
+const wrapper = flex({
+    flexDirection: "column",
+    padding: "layoutX",
+    margin: 0,
+    overflowX: "hidden",
+    overflowY: "auto",
+    gap: 0,
+    minHeight: "100vh",
+});
+
+const container = flex({
+    flexDirection: "column",
+    position: "relative",
+    backgroundColor: "whiteBackground",
+    flex: 1,
+});
+
+const localeToLang: { [key: string]: string } = {
+    "en-US": "en",
+    "ko-KR": "ko",
+    "ja-JP": "ja",
+    "zh-CN": "zh-CN",
+    "zh-TW": "zh-TW",
+};
+
+export default async function LocaleLayout({
+    children,
+    params,
+}: LocaleLayoutProps) {
+    const { locale } = await params;
+    const htmlLang = localeToLang[locale] || "en";
+
+    return (
+        <html lang={htmlLang} className={`${Paperlogy.className}`}>
+            <body className={wrapper}>
+                <SpeedInsights />
+                <StoreInitializer />
+                {/* <LocaleInitializer /> */}
+                <TouchProvider>
+                    <Header locale={locale} />
+                    <main className={container}>{children}</main>
+                </TouchProvider>
+            </body>
+        </html>
+    );
+}
